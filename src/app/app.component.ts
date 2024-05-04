3202b148d3b94b0b99238a418b70a3623f551c61
Ditha Raju <ditharaju91@yahoo.com>
2024-05-04 09:37:29 +0400

    intro section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ServicesOfferedComponent } from './services-offered/services-offered.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { IntroComponent } from './intro/intro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, AboutMeComponent, ServicesOfferedComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, IntroComponent, AboutMeComponent, ServicesOfferedComponent, ContactsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-contacts',
+  selector: 'contacts',
   standalone: true,
   imports: [],
   templateUrl: './contacts.component.html',
